49b6c0918cd15d568ec74664b9c6a9fd03787de6
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-11 12:35:53 +0800

    [Jeffery] Finish again

--- src/main/java/com/thoughtworks/tdd/FizzBuzzWhizz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzWhizz.java
@@ -1,4 +1,20 @@
 package com.thoughtworks.tdd;
 
 public class FizzBuzzWhizz {
+    public String FizzBuzzWhizzWithNumber(int number) {
+        StringBuffer result = new StringBuffer();
+        if(String.valueOf(number).indexOf('3')!=-1){
+            return "Fizz";
+        }
+        if(number % 3 == 0){
+            result.append("Fizz");
+        }
+        if(number % 5 == 0){
+            result.append("Buzz");
+        }
+        if(number % 7 == 0){
+            result.append("Whizz");
+        }
+        return result.toString().equals("")?String.valueOf(number):result.toString();
+    }
 }
